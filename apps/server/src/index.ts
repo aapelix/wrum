67f1f453d16b38f3c55319b6e24938f1892c40b9
aapelix <aaro@tuta.com>
2026-03-29 12:46:40 +0300

    server process.env.PORT

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -69,7 +69,7 @@ server.route({
 });
 
 try {
-  await server.listen({ port: 3000 });
+  await server.listen({ port: process.env.PORT ? parseInt(process.env.PORT) : 3000 });
 } catch (err) {
   server.log.error(err);
   process.exit(1);
