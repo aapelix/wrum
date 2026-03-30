ca39ee53623e9090ec92c0003157bf02187527d1
aapelix <aaro@tuta.com>
2026-03-30 19:11:06 +0300

    change routes url

--- apps/game/src/auth/client.ts
+++ apps/game/src/auth/client.ts
@@ -2,9 +2,8 @@ import { createAuthClient } from "better-auth/client";
 import { anonymousClient, emailOTPClient } from "better-auth/client/plugins";
 
 const apiUrl = import.meta.env.VITE_API_URL;
-const authPath = `${apiUrl}/auth`;
 
 export const authClient = createAuthClient({
-  baseURL: authPath,
+  baseURL: apiUrl,
   plugins: [emailOTPClient(), anonymousClient()],
 });
