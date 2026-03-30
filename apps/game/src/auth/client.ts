7f050a90ba9acf5ba26f9e18ba17e3b042e297ec
aapelix <aaro@tuta.com>
2026-03-30 17:54:09 +0300

    someone smart though it would be funny to make it /api/api/ (not me 100%)

--- apps/game/src/auth/client.ts
+++ apps/game/src/auth/client.ts
@@ -2,9 +2,9 @@ import { createAuthClient } from "better-auth/client";
 import { anonymousClient, emailOTPClient } from "better-auth/client/plugins";
 
 const apiUrl = import.meta.env.VITE_API_URL;
+const authPath = `${apiUrl}/api/auth`;
 
 export const authClient = createAuthClient({
-  baseURL: apiUrl,
-  basePath: "/api/auth",
+  baseURL: authPath,
   plugins: [emailOTPClient(), anonymousClient()],
 });
